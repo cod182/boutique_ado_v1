6ff0df7b9e130a5b70c7112760c8ebddc24b6d0d
Codie Stephens-Evans <codie18@gmail.com>
2021-10-16 15:41:21 +0000

    added products app, models and fixtures

--- home/views.py
+++ home/views.py
@@ -2,11 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def index(request):
-    """[summary]
+    """ A view to return the index page """
 
-    Returns:
-        [html]: [view to return index page]
-    """
     return render(request, 'home/index.html')
