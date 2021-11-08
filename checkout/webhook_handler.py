bc257e4e75d8f982f2ed0e8c8c630276af9ccfa8
Codie Stephens-Evans <codie18@gmail.com>
2021-11-08 17:14:38 +0000

    webhook setup

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-""" Handle Stripe Webhooks"""
-class StraipWH_Handler:
+
+
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
@@ -11,23 +13,20 @@ class StraipWH_Handler:
         """
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
-            status=200
-        )
+            status=200)
 
     def handle_payment_intent_succeeded(self, event):
         """
-        Handle a payment_intent.succeeded webhook event
+        Handle the payment_intent.succeeded webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
-            status=200
-        )
+            status=200)
 
     def handle_payment_intent_payment_failed(self, event):
         """
-        Handle a payment_intent.payment_failed webhook event
+        Handle the payment_intent.payment_failed webhook from Stripe
         """
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
-            status=200
-        )
+            status=200)
--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -3,29 +3,27 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 
-from checkout.webhook_handler import StraipWH_Handler
+from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
 @require_POST
 @csrf_exempt
 def webhook(request):
-    """ Listen for webhooks from Stripe """
+    """Listen for webhooks from Stripe"""
     # Setup
-
-    wh_secret = settings.STRIP_WH_SECRET
+    wh_secret = settings.STRIPE_WH_SECRET
     stripe.api_key = settings.STRIPE_SECRET_KEY
 
-    # Get the webhook data and verity it's signature
-
+    # Get the webhook data and verify its signature
     payload = request.body
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret
-            )
+        payload, sig_header, wh_secret
+        )
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -34,8 +32,5 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
+
     
-    print('success!')
-    return HttpResponse(
-        status = 200
-    )
