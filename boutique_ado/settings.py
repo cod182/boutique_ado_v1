df61442786b288985c8f8233f91ed41408c0836b
Codie Stephens-Evans <codie18@gmail.com>
2021-11-23 16:46:40 +0000

    SC update

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2qd4c&ja5i5*g*fx@ylr$!)9bsem8j=ck53@9#@l-%l+m0v!r5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cod-boutique.herokuapp.com', 'localhost']
 
