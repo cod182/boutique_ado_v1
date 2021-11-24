8ae96db65306390b85eaea0020b0aa78bf6b3fd0
Codie Stephens-Evans <codie18@gmail.com>
2021-11-24 17:00:39 +0000

    s3 set up

---
+++ custom_storages.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+from storage.backends.s3boto3 import S3Boto3Storages
+
+class StaticStorage(S3Boto3Storages):
+    location = settings.STATICFILES_LOCATION
+    
+class MediaStorage(S3Boto3Storages):
+    location = settings.MEDIAFILES_LOCATION
